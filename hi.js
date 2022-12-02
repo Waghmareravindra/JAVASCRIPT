ed406479c75df377f49c0bce41a9af976f917510
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2022-12-02 15:44:25 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,94 @@
+// console.log(typeof "Ravindra" );
+// console.log(typeof 234550);
+// console.log(typeof 23569599699);
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+
+
+// var
+
+// canst x='Ravindra';
+// var y='is';
+// let z='started';
+
+
+
+// const a=10;
+
+// const b=20;
+
+// if(a<b){
+//     console.log('a is small')
+// } else {
+//     console.log(' b is greater')
+// }
+
+
+
+// const n=10;
+
+// for (let i=0; i<n; i++){
+//     console.log(i);
+// }
+
+
+// Error Handling
+
+
+// try{
+//     throw new error("Ravindra is Boring");   / CREATE ERROR /
+// }catch (err){
+//     console.log("Ravindra is intersted to do code :)");    / HERE ERROR WE CAN SOLVE /
+// }
+
+
+
+
+
+
+// Functions 
+
+
+
+// function square(x){
+//     return x*x*x*x;
+// }
+// console.log(square(2));
+
+
+// npm i cowsay
+
+
+
+
+
+// npm init              project 
+// npm i cowsay 
+
+// const cowsay = require("cowsay");
+
+// console.log(cowsay.say({
+//     text : "I'm a Ravindra", // always n this string 
+//     e : "oO",
+//     T : "U "
+// }));
+
+
+// array 
+// json
+
+// const a=['ravindra', 123456789, undefined]
+
+
+// //  key - value pair
+
+// const jsonr = {
+//     'name':'ravindra',
+//     'surname':'waghmare',
+//     'food_liked':['sweet','misal']
+// }
+// console.log(jsonr.surnsme)
